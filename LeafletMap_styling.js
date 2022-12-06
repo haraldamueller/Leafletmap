226f5bc5eb7114583c899ffbdfea56f28619eccd
haraldamueller <harald.mueller@gmx.de>
2022-12-06 19:18:49 +0100

    Update LeafletMap_styling.js

--- LeafletMap_styling.js
+++ LeafletMap_styling.js
@@ -15,33 +15,34 @@
 	 </fieldset>
 	 </form>
  `;
+ 
  class LeafletMapStylingPanel extends HTMLElement {
 	constructor() {
-	super();
-	this._shadowRoot = this.attachShadow({mode: "open"});
-	this._shadowRoot.appendChild(template.content.cloneNode(true));
+		super();
+		this._shadowRoot = this.attachShadow({mode: "open"});
+		this._shadowRoot.appendChild(template.content.cloneNode(true));
 
-	this._shadowRoot.getElementById("form").addEventListener("submit",
-	this._submit.bind(this));
- }
+		this._shadowRoot.getElementById("form").addEventListener("submit",
+		this._submit.bind(this));
+	 }
 
- _submit(e) {
-	e.preventDefault();
-	this.dispatchEvent(new CustomEvent("propertiesChanged", {
-	detail: {
-		properties: {
-		dataUrl: this.dataUrl
+	_submit(e) {
+		e.preventDefault();
+		this.dispatchEvent(new CustomEvent("propertiesChanged", {
+			detail: {
+				properties: {
+				dataUrl: this.dataUrl
+				}
+			}
+		}));
+	}
+	set dataUrl(newUrl) {
+		this._shadowRoot.getElementById("data_url").value = newUrl;
+	}
+ 
+	get dataUrl() {
+		return this._shadowRoot.getElementById("data_url").value;
 	}
  }
-})
-);
-
- }
- set dataUrl(newUrl) {
- this._shadowRoot.getElementById("data_url").value = newUrl;
- }
- get dataUrl() {
- return this._shadowRoot.getElementById("data_url").value;
- }
- }
+ 
 customElements.define("com-sap-sample-schwarz-leaflet_map-styling", LeafletMapStylingPanel);
